b6dbaf2127df9e8b7461f3536f4b785c2f5e9c07
Marcos de Oliveira <marcosdeoliveira@MacBook-Pro-de-Marcos.local>
2020-08-14 19:28:31 -0300

    adiciona produto no carrinho

--- src/app/components/loja/produto-card/produto-card.component.ts
+++ src/app/components/loja/produto-card/produto-card.component.ts
@@ -1,3 +1,5 @@
+import { ToastrService } from 'ngx-toastr';
+import { CartUtil } from './../../../utils/cart.util';
 import { Produto } from './../../../models/produto.model';
 import { Component, OnInit, Input } from '@angular/core';
 
@@ -9,9 +11,20 @@ export class ProdutoCardComponent implements OnInit {
 
   @Input() produto: Produto;
 
-  constructor() { }
+  constructor(private toast: ToastrService) { }
 
   ngOnInit(): void {
+  }
+
+  public adiciona(): void {
+    CartUtil.add(
+      this.produto._id,
+      this.produto.title,
+      1,
+      this.produto.price,
+      this.produto.images[0]
+    );
+    this.toast.success(`Item adicionado com sucesso! ${this.produto.title}`);
   }
 
 }
