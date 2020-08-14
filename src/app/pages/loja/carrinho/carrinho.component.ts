b318f6b458efc5ecdc180268a95d51b1d28e1096
Marcos de Oliveira <marcosdeoliveira@MacBook-Pro-de-Marcos.local>
2020-08-14 20:05:58 -0300

    commit final

--- src/app/pages/loja/carrinho/carrinho.component.ts
+++ src/app/pages/loja/carrinho/carrinho.component.ts
@@ -1,4 +1,6 @@
+import { CartUtil } from './../../../utils/cart.util';
 import { Component, OnInit } from '@angular/core';
+import { Cart } from 'src/app/models/cart.model';
 
 @Component({
   selector: 'app-carrinho',
@@ -6,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CarrinhoComponent implements OnInit {
 
+  public cart: Cart;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.loadCarrinho();
+  }
+
+  public loadCarrinho(): void {
+    this.cart = CartUtil.get();
+  }
+
+  public removeItem(item): void {
+    const index = this.cart.items.indexOf(item);
+    this.cart.items.splice(index, 1);
+    CartUtil.update(this.cart);
+  }
+
+  public clear(): void {
+    CartUtil.clear();
+    this.loadCarrinho();
+  }
+
+  public total(): number {
+    let total = 0;
+
+    this.cart.items.forEach(item => {
+      total += item.price * item.quantity;
+    });
+
+    return total;
   }
 
 }
